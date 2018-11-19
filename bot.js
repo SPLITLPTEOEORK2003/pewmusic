f80da464ac0ef4cb2802ed9ab400e8879cb3d846
Sellmox5 <42848880+SPLITLPTEOEORK2003@users.noreply.github.com>
2018-11-19 16:48:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Pplay | YouTube`,"http://twitch.tv/Death Shop")
+client.user.setGame(` I will survive and kill you`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
